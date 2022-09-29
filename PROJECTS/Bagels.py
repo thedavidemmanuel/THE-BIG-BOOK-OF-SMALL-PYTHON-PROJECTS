83dcbbd87fd9807816ec9194832118c457433eca
David Emmanuel <sirdavidemmanuel@gmail.com>
2022-09-30 00:08:17 +0200

    Projects

--- PROJECTS/Bagels.py
+++ PROJECTS/Bagels.py
@@ -7,7 +7,7 @@ Tags: short, game, puzzle"""
 
 import random
 
-NUM_DIGITS = 4  # (!) Try setting this to 1 or 10.
+NUM_DIGITS = 3  # (!) Try setting this to 1 or 10.
 MAX_GUESSES = 10  # (!) Try setting this to 1 or 100.
 
 
--- PROJECTS/BirthdayParadox.py
+++ PROJECTS/BirthdayParadox.py
@@ -1,7 +1,7 @@
 """Birthday Paradox Simulation, by Al Sweigart al@inventwithpython.com
 Explore the surprising probabilities of the "Birthday Paradox".
 More info at https://en.wikipedia.org/wiki/Birthday_problem
-View this code at https://nostarch.com/big-book-small-python-projects
+This code is available at https://nostarch.com/big-book-small-python-programming
 Tags: short, math, simulation"""
 
 import datetime, random
@@ -26,23 +26,23 @@ def getMatch(birthdays):
     """Returns the date object of a birthday that occurs more than once
     in the birthdays list."""
     if len(birthdays) == len(set(birthdays)):
-        return None # All birthdays are unique, so return None.
+        return None  # All birthdays are unique, so return None.
 
     # Compare each birthday to every other birthday:
     for a, birthdayA in enumerate(birthdays):
         for b, birthdayB in enumerate(birthdays[a + 1 :]):
             if birthdayA == birthdayB:
-                return birthdayA # Return the matching birthday.
- 
+                return birthdayA  # Return the matching birthday.
 
- # Display the intro:
+
+# Display the intro:
 print('''Birthday Paradox, by Al Sweigart al@inventwithpython.com
- 
- The Birthday Paradox shows us that in a group of N people, the odds
- that two of them have matching birthdays is surprisingly large.
- This program does a Monte Carlo simulation (that is, repeated random
- simulations) to explore this concept.
- 
+
+The birthday paradox shows us that in a group of N people, the odds
+that two of them have matching birthdays is surprisingly large.
+This program does a Monte Carlo simulation (that is, repeated random
+simulations) to explore this concept.
+
 (It's not actually a paradox, it's just a surprising result.)
 ''')
 
@@ -50,12 +50,12 @@ print('''Birthday Paradox, by Al Sweigart al@inventwithpython.com
 MONTHS = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
           'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
 
-while True: # Keep asking until the user enters a valid amount.
+while True:  # Keep asking until the user enters a valid amount.
     print('How many birthdays shall I generate? (Max 100)')
     response = input('> ')
     if response.isdecimal() and (0 < int(response) <= 100):
         numBDays = int(response)
-        break # User has entered a valid amount.
+        break  # User has entered a valid amount.
 print()
 
 # Generate and display the birthdays:
@@ -77,7 +77,7 @@ match = getMatch(birthdays)
 # Display the results:
 print('In this simulation, ', end='')
 if match != None:
-    monthName = MONTHS[match.month - 1] 
+    monthName = MONTHS[match.month - 1]
     dateText = '{} {}'.format(monthName, match.day)
     print('multiple people have a birthday on', dateText)
 else:
@@ -89,10 +89,10 @@ print('Generating', numBDays, 'random birthdays 100,000 times...')
 input('Press Enter to begin...')
 
 print('Let\'s run another 100,000 simulations.')
-simMatch = 0 # How many simulations had matching birthdays in them.
-for i in range(100_000):
+simMatch = 0  # How many simulations had matching birthdays in them.
+for i in range(100000):
     # Report on the progress every 10,000 simulations:
-    if i % 10_000 == 0:
+    if i % 10000 == 0:
         print(i, 'simulations run...')
     birthdays = getBirthdays(numBDays)
     if getMatch(birthdays) != None:
@@ -100,7 +100,7 @@ for i in range(100_000):
 print('100,000 simulations run.')
 
 # Display simulation results:
-probability = round(simMatch / 100_000 * 100, 2)
+probability = round(simMatch / 100000 * 100, 2)
 print('Out of 100,000 simulations of', numBDays, 'people, there was a')
 print('matching birthday in that group', simMatch, 'times. This means')
 print('that', numBDays, 'people have a', probability, '% chance of')
